9f7e642d88c15fc57a81c637312f729678d416ba
Nino Aguilar <ninoaguilar95@gmail.com>
2019-02-25 18:13:20 -0700

    Upgraded to from dotnet 2.1 --> 2.2

--- LocateMeService/CompanyService/Startup.cs
+++ LocateMeService/CompanyService/Startup.cs
@@ -32,15 +32,12 @@ namespace LocateMeApi
 
             services.AddMvc().AddJsonOptions(options => {
                 options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
-            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
             using (var serviceScope = app.ApplicationServices.CreateScope())
             {
                 var context = serviceScope.ServiceProvider.GetService<CompanyServiceContext>();
